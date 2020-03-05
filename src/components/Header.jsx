e51fb9d9f8a43493ae4137b4e29985fd9c545bb5
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-03-05 15:54:59 -0800

    add component folder and basics

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import styled from 'styled-components';
+import Body from './Body';
+
+function Header() {
+
+}
+
+export default Header;
