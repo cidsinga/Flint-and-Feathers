8a7fb37335bf392270c0f7c6469e4e5b643f2a5f
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-03-06 10:27:41 -0800

    add grid basics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,21 @@ import { Router } from 'react-router-dom';
 
 function Header() {
 
+  const container = {
+    display: 'grid',
+    gridTemplateColumns: '300px 300px 300px',
+  }
+
   const menu = {
-    display: "inline-block",
+
+  }
+
+  const flint = {
+
   }
   return (
-    <div>
-    <h1 style>flint and feathers</h1>
+    <div style={container}>
+    <h1 style={flint}>flint and feathers</h1>
       <div stlye={menu}>
       <Link to='/'><h3>Collections</h3></Link>
       <Link to='/'><h3>About</h3></Link>
