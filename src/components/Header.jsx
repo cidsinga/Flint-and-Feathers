49da25480b5ca92aa4f696fd8ddb41eee8226125
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-03-06 10:48:56 -0800

    add wrestling with inline block

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Router } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Router } from "react-router-dom";
 
 function Header() {
-
   const container = {
-    display: 'grid',
-    gridTemplateColumns: '300px 300px 300px',
-  }
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 1fr",
+    padding: "3%"
+  };
 
   const menu = {
+    display: "inline"
+  };
 
-  }
-
-  const flint = {
-
-  }
+  const flint = {};
   return (
     <div style={container}>
-    <h1 style={flint}>flint and feathers</h1>
-      <div stlye={menu}>
-      <Link to='/'><h3>Collections</h3></Link>
-      <Link to='/'><h3>About</h3></Link>
-      <Link to='/'><h3>Gallery</h3></Link>
-      <Link to='/'><h3>Shop</h3></Link>
+      <div>
+        <h1 style={flint}>flint and feathers</h1>
       </div>
-    <div>
-    </div>
+      <div></div>
 
-
+      <div stlye={menu}>
+        <Link to="/">
+          <h3>Collections</h3>
+        </Link>
+        <Link to="/">
+          <h3>About</h3>
+        </Link>
+        <Link to="/">
+          <h3>Gallery</h3>
+        </Link>
+        <Link to="/">
+          <h3>Shop</h3>
+        </Link>
+      </div>
+      <div></div>
     </div>
   );
-
 }
-
 
 export default Header;
