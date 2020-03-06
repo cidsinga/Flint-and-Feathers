251650d8a06f6ecf707eb3628495f01cda5dfda9
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-03-06 09:43:15 -0800

    add study and basics of navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,14 @@ import { Router } from 'react-router-dom';
 
 
 function Header() {
+
+  const menu = {
+    display: "inline-block",
+  }
   return (
     <div>
-    <h1>flint and feathers</h1>
-      <div>
+    <h1 style>flint and feathers</h1>
+      <div stlye={menu}>
       <Link to='/'><h3>Collections</h3></Link>
       <Link to='/'><h3>About</h3></Link>
       <Link to='/'><h3>Gallery</h3></Link>
