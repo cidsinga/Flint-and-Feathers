eb74f7e014e78ca61fa0a2cd876295cd8a1f52c2
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-03-11 10:30:13 -0700

    add flowers gallery

--- src/components/Flowers.jsx
+++ src/components/Flowers.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Router } from "react-router-dom";
 import styled from "styled-components";
+import { Container, Row, Col } from "reactstrap";
+import flowers from "./Assets/flowersMock.jpg";
+import monroe from "./Assets/Monroe.jpg";
+import newBeginningsFlowers from "./Assets/NewBeginningsFlowers.jpg";
+import grey from "./Assets/PaynesGreyFloral.jpg";
+import poppies from "./Assets/Poppies.jpg";
+import spring from "./Assets/SpringFlowers.jpg";
+import summer from "./Assets/SummerFlowers.jpg";
 
 function Flowers() {
   const container = {
@@ -16,11 +24,46 @@ function Flowers() {
   };
 
   const photo = {
-    width: "80%"
+    width: "90%"
   };
+
+  const left = {
+    display: "inline-block",
+    float: "left",
+    width: "50%"
+  };
+
+  const right = {
+    display: "inline-block",
+    float: "right",
+    width: "50%"
+  };
+
+  const row = {
+    width: "100%"
+  };
+
   return (
     <div style={container}>
-      <h1>Flowers</h1>
+      <Container>
+        <h1>Flowers</h1>
+        <Row style={row}>
+          <Col xs="auto" style={left}>
+            <img style={photo} src={flowers} />
+            <img style={photo} src={spring} />
+            <img style={photo} src={grey} />
+          </Col>
+          <Col xs="auto" style={right}>
+            <img style={photo} src={summer} />
+            <img style={photo} src={poppies} />
+            <img style={photo} src={newBeginningsFlowers} />
+          </Col>
+        </Row>
+        <row>
+          <img style={photo} src={monroe} />
+        </row>
+        <Row></Row>
+      </Container>
     </div>
   );
 }
